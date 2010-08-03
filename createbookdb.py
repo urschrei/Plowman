c86297e9f7770546e646a7b45a70aac92a4816d8
steph <urschrei@gmail.com>
2010-08-03 23:58:15 +0100

    Added some very basic error handling, and prettified output

--- createbookdb.py
+++ createbookdb.py
@@ -1,7 +1,11 @@
 import sys
 import os
 import sqlite3
-connection = sqlite3.connect('dc.db')
+try:
+	connection = sqlite3.connect('dc.db')
+except IOError:
+	print("Couldn't connect to the SQLite db. Exiting.")
+	sys.exit()
 cursor = connection.cursor()
 # cursor.execute('CREATE TABLE position (id INTEGER PRIMARY KEY, position INTEGER, tweeted INTEGER)')
 # This is useful: http://openbookproject.net//thinkCSpy/
@@ -11,26 +15,27 @@ cursor = connection.cursor()
 # If not, then don't try setting row = cursor, or any of that other stuff
 # The implication is that the script hasn't been run before
 # Just set lastline = 0, then continue
-
 cursor.execute('SELECT position FROM position ORDER BY POSITION DESC LIMIT 1')
+# get the highest page number
 row = cursor.fetchone()
-
 lastline = row[0]
-print(lastline+1)
-
-
+printline = lastline+1
+#print('l. ' + str(printline) + '')
 print_this = list()
-with open('dc.txt', encoding='utf-8') as t_file:
-	for a_line in t_file:
-		if not a_line.strip():
-			continue
-			# if we encounter a blank line, do nothing and carry on
-		else:
-			print_this.append(a_line)
-			# we could just jump to a specific line in the file, but that appears to be tricky in Python
+try:
+	with open('dc.txt', encoding='utf-8') as t_file:
+		for a_line in t_file:
+			if not a_line.strip():
+				continue
+				# if we encounter a blank line, do nothing and carry on
+			else:
+				print_this.append(a_line)
+				# we could just jump to a specific line in the file, but that appears to be tricky in Python
+except IOError:
+	print("Couldn't open the text file for reading. Exiting.")
+	sys.exit()
 # substitute some form of tweeting mechanism for the next line
-# maybe also prettify it, e.g. l.123: bla bla bla
-print(print_this[lastline])
+print('l. ' + str(printline) + ': ' + print_this[lastline])
 # if it's successful, execute the next three lines, else exit
 lastline = lastline + 1
 cursor.execute('INSERT INTO position VALUES (null, ?, ?)',(lastline, "1"))
