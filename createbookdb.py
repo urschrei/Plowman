d050ddd76f844e6c0405cbef5f184e1602ae7d64
steph <urschrei@gmail.com>
2010-08-03 22:50:25 +0100

    More to-do comments

--- createbookdb.py
+++ createbookdb.py
@@ -4,7 +4,7 @@ import sqlite3
 connection = sqlite3.connect('dc.db')
 cursor = connection.cursor()
 # cursor.execute('CREATE TABLE position (id INTEGER PRIMARY KEY, position INTEGER, tweeted INTEGER)')
-
+# This is useful: http://openbookproject.net//thinkCSpy/
 # TO-DO:
 # check if the following operation is successful.
 # If not, then don't try setting row = cursor, or any of that other stuff
@@ -15,8 +15,7 @@ cursor.execute('SELECT position FROM position ORDER BY POSITION DESC LIMIT 1')
 row = cursor.fetchone()
 
 lastline = row[0]
-lastline = lastline + 1
-print(lastline)
+print(lastline+1)
 
 
 print_this = list()
@@ -27,7 +26,11 @@ with open('dc.txt', encoding='utf-8') as t_file:
 			# if we encounter a blank line, do nothing and carry on
 		else:
 			print_this.append(a_line)
-
+			# we could just jump to a specific line in the file, but that appears to be tricky in Python
+# substitute some form of tweeting mechanism for the next line
+# maybe also prettify it, e.g. l.123: bla bla bla
 print(print_this[lastline])
+# if it's successful, execute the next three lines, else exit
+lastline = lastline + 1
 cursor.execute('INSERT INTO position VALUES (null, ?, ?)',(lastline, "1"))
 connection.commit()
