7b72af8405b685dac07cc9064e1e3005c8d2dd0b
steph <urschrei@gmail.com>
2010-08-04 01:47:29 +0100

    Integrated yatcip stuff, which doesn't work yet

--- bookbyline.py
+++ bookbyline.py
@@ -1,16 +1,26 @@
+#!/usr/local/bin/python3
 import sys
 import os
 import sqlite3
+sys.path.append("/Users/sth/scripts")
+import yatcip
 connection = sqlite3.connect('dc.db')
 cursor = connection.cursor()
+
 # cursor.execute('CREATE TABLE position (id INTEGER PRIMARY KEY, position INTEGER, tweeted INTEGER)')
+
 # This is useful: http://openbookproject.net//thinkCSpy/
 # This, too: http://www.devshed.com/c/a/Python/Using-SQLite-in-Python/
+
 # TO-DO:
 # check if the following operation is successful.
 # If not, then don't try setting row = cursor, or any of that other stuff
 # The implication is that the script hasn't been run before
 # Just set lastline = 0, then continue
+
+# twitter: robo_dante/beatrice
+# gmail alighieribot2010/beatrice1265
+
 try:
 	cursor.execute('SELECT position FROM position ORDER BY POSITION DESC LIMIT 1')
 except sqlite3.OperationalError:
@@ -33,9 +43,26 @@ try:
 except IOError:
 	print("Couldn't open the text file for reading. Exiting.")
 	sys.exit()
-# substitute some form of tweeting mechanism for the next line
-print('l. ' + str(printline) + ': ' + print_this[lastline])
-# if it's successful, execute the next three lines, else exit
+
+# Twitter yatcip stuff
+bot = yatcip.Twitter('dante_bot', 'beatrice')
+
+response = bot.update('my new post')
+# response is a twitter post, so response['text'] == 'my new post'
+
+for dm in bot.direct_messages():
+    bot.direct_message('Thanks for your message!', dm['sender']['id'])
+
+post='l. ' + str(printline) + ': ' + print_this[lastline]
+# check for exceptions:
+try:
+    bot.post(post)
+except:
+	print("Something's gone wrong…")
+	sys.exit()
+# end of yatcip stuff
+
+# if it's successful, execute the next three lines
 lastline = lastline + 1
 cursor.execute('INSERT INTO position VALUES (null, ?, ?)',(lastline, "1"))
 connection.commit()
