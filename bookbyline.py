1b39181ea00c9d6837347a229e25639e65e2efc4
steph <urschrei@gmail.com>
2010-08-09 15:29:21 +0100

    non-live

--- bookbyline.py
+++ bookbyline.py
@@ -160,7 +160,8 @@ class BookFromTextFile:
 				logging.error(now.strftime("%Y-%m-%d %H:%M") + " " + \
 				str(sys.argv[0]) + " " + "Couldn't update the DB")
 			try:
-				api.update_status(str(self.lines[-1]))
+				#api.update_status(str(self.lines[-1]))
+				print self.lines[-1]
 			except tweepy.TweepError , err:
 				logging.error(now.strftime("%Y-%m-%d %H:%M") + " " + \
 				str(sys.argv[0]) + " " + "Couldn't update status. " + \
