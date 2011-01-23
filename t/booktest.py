62bf05e1701fbf575841552807d5075b235ca6cf
Stephan Hügel <urschrei@gmail.com>
2011-01-23 19:16:55 +0000

    Added some setup options, and a test for text file formatting

--- t/booktest.py
+++ t/booktest.py
@@ -1,21 +1,36 @@
 #! /usr/bin/env python
 # coding=utf-8
 
-import bookbyline
 import unittest
+import sys
+sys.path.insert(0, '..')
+
+import bookbyline
 
 class KnownValues(unittest.TestCase):
-    knownValues = ((['a', 'b', 'c', 'd', 'e'], '03de6c570bfe24bfc328ccd7ca46b76eadaf4334'),
+
+    # provide known correct SHA1 hash of a list of strings
+    knownValues = ((['a', 'b', 'c', 'd', 'e'],
+    '03de6c570bfe24bfc328ccd7ca46b76eadaf4334'),
     )
+    # read lines from a test text file
+    with open('test_file.txt', 'r') as f:
+        lines = f.readlines()
 
 
     def testBookByLineKnownValues(self):
-        """ get_hash should give known result with known input
+        """ Should return a SHA1 hash for a given list of strings
         """
-        for string, sha_hash in self.knownValues
+        for string, sha_hash in self.knownValues:
             result = bookbyline.get_hash(string)
             self.assertEqual(sha_hash, result)
 
+
+    def testBookByLineImpFile(self):
+        """ Should return a tuple containing no blank lines
+        """
+        result = bookbyline.imp_file(self.lines)
+        self.assertTrue(type(result) == tuple)
 
 
 
